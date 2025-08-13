f5811da677497c03560af2b80f9184ae118f071a
leon <167691743+S0MBRA99@users.noreply.github.com>
2025-08-13 17:56:08 +0000

    add conditionals

--- src/js/app.js
+++ src/js/app.js
@@ -34,15 +34,15 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
           ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.name}${variables.lastName}@gmail.com</h2>
-          <h3>${variables.role}</h3>
-          <h3>${variables.city} ${variables.country}</h3>
+          <h1>${variables.name == null? variables.name = 'name': variables.name} ${variables.name == null? variables.lastName = 'lastname': variables.lastName}</h1>
+          <h2>${variables.name == null? variables.name = 'name': variables.name}${variables.name == null? variables.lastName = 'lastname': variables.lastName}@gmail.com</h2>
+          <h3>${variables.role == null? variables.role = 'role': variables.role}</h3>
+          <h3>${variables.city == null? variables.city = 'city': variables.city} ${variables.country == null? variables.country = 'city': variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${variables.twitter == null? variables.twitter = 'city': variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${variables.github == null? variables.github = 'city': variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${variables.linkedin == null? variables.linkedin = 'city': variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${variables.city == null? variables.city = 'city': variables.city}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
